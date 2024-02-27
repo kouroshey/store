6b0f6447b0692e66db60890dbbcbfa37bfbf4ae6
ImAp68 <iman.azizpour68@gmail.com>
2024-02-27 13:38:34 -0800

    footer-80%

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,7 +5,7 @@ import Footer from './Footer/Footer';
 const Layout = ({ children }) => {
   return (
     <div className="bg-image bg-cover bg-no-repeat bg-center" style={{ backgroundImage: "url('./assets/mainBg.png')" }}>
-      {/* <Header /> */}
+      <Header />
       <Footer />
       {children}
     </div>
