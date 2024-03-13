ecd1f44c2165683a5e81841bb964aad5ed3aab80
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2024-03-13 21:08:02 +0330

    changes on carousel

--- src/components/common/Carousel-img.jsx
+++ src/components/common/Carousel-img.jsx
@@ -1,36 +1,42 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/prop-types */
 import { useEffect, useState } from 'react';
 import { FaChevronLeft } from 'react-icons/fa';
 import { FaChevronRight } from 'react-icons/fa';
 
 export default function Carousel({ slides }) {
+  const [isHover, setIsHover] = useState(false)
   const [curr, setCurr] = useState(0);
-  console.log(curr);
+
+  const hoverHandler = () => {
+    setIsHover(prev => !prev)
+  }
+
   const preImag = () => {
     return setCurr((curr) => (curr === 0 ? slides.length - 1 : curr - 1));
   };
+
   const nextImag = () => {
     return setCurr((curr) => (curr === slides.length - 1 ? 0 : curr + 1));
   };
+
   useEffect(() => {
-    const slideInter = setInterval(nextImag, 2000);
-    return () => clearInterval(slideInter);
-  }, []);
+    if (!isHover) {
+      const slideInter = setInterval(nextImag, 3000);
+      return () => clearInterval(slideInter);
+    }
+  }, [isHover]);
 
   return (
-    <div className="overflow-hidden grid grid-cols-[repeat(20,100%)] h-full   relative ">
-      {slides.map((item, index) => {
-        return (
-          <div
-            key={index}
-            className={` flex justify-center h-full w-full items-center text-6xl font-bold transition-all  ease-out duration-700`}
-            style={{ transform: `translateX(-${curr * 100}%)` }}
-          >
-            {item}
-          </div>
-        );
-      })}
+    <div onMouseEnter={hoverHandler} onMouseLeave={hoverHandler} className="overflow-hidden grid grid-cols-[repeat(20,100%)] h-full relative ">
+      {slides.map((item, index) => (
+        <div
+          key={index}
+          className={` flex justify-center h-full w-full items-center text-6xl font-bold transition-all  ease-out duration-700`}
+          style={{ transform: `translateX(-${curr * 100}%)` }}
+        >
+          {item}
+        </div>
+      )
+      )}
       <div className="top-0 absolute flex  w-full justify-between text-2xl  h-full  items-center">
         <button className="h-full w-28 flex justify-center items-center " onClick={preImag}>
           <FaChevronLeft />
