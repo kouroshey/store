79728271a9aa3e23045f780e404f7a788a835fc9
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2024-03-19 10:10:50 +0330

    make dynamic button and card slide

--- src/components/common/ImageSlider/CardSlide.jsx
+++ src/components/common/ImageSlider/CardSlide.jsx
@@ -1,19 +1,33 @@
 /* eslint-disable no-unused-vars */
 import { Button } from '../Button';
 
-const CardSlide = ({ title, img, url, desc, subtitle }) => {
+const CardSlide = ({
+  title,
+  img,
+  url,
+  desc,
+  subtitle,
+  buttonStyle,
+  btnUrl
+}) => {
   return (
-    <div className="flex  w-full h-full  justify-center gap-32">
-      <div className="flex flex-col gap-2  justify-center">
-        <div className="text-xl font-medium  text-white">{title}</div>
-        <h2 className="text-3xl font-semibold text-white">{subtitle}</h2>
-        <p className="text-xs">{desc}</p>
-        <div className="w-24 mt-2">
-          <Button btnEffect={true} url={url} textBtn={'SHOP NOW'} />
-        </div>
-      </div>
-      <div className="flex py-10">
+    <div className="flex flex-col items-center lg:flex-row w-full justify-center gap-32">
+      <div className="flex py-10 lg:order-2">
         <img src={img} alt="" />
+      </div>
+      <div className="flex flex-col gap-8 lg:order-1 justify-center">
+        <div className='flex flex-col gap-2'>
+          <p className="text-xl font-medium  text-white">{title}</p>
+          <p className="text-4xl font-semibold text-white">{subtitle}</p>
+          <p className="text-xs">{desc}</p>
+        </div>
+        <Button
+          buttonStyle={buttonStyle}
+          text={'SHOP NOW'}
+          btnUrl={btnUrl}
+          isLink={true}
+          url={url}
+        />
       </div>
     </div>
   );
