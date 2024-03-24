d2d733f84abf15bf8eb9d05976baf0bbbcec874f
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2024-03-24 12:41:34 +0330

    fix issues

--- src/components/common/Tab/Tab.jsx
+++ src/components/common/Tab/Tab.jsx
@@ -1,31 +1,31 @@
 import React, { useState } from 'react';
 
 const Tab = () => {
-  const [activeTab, setActiveTab] = useState(1);
-  const handlerTabHeadr = (index) => {
-    setActiveTab(index);
-  };
-  return (
-    <div>
-      <div className="flex ">
-        <div  className={` flex w-8 ${activeTab == 1 ? 'bg-green-400' : 'bg-slate-400'} px-12 py-2 items-center justify-center`} onClick={()=>handlerTabHeadr(1)}>
-          Tab1
+    const [activeTab, setActiveTab] = useState(1);
+    const handlerTabHeadr = (index) => {
+        setActiveTab(index);
+    };
+    return (
+        <div>
+            <div className="flex ">
+                <div className={` flex w-8 ${activeTab == 1 ? 'bg-green-400' : 'bg-slate-400'} px-12 py-2 items-center justify-center`} onClick={() => handlerTabHeadr(1)}>
+                    Tab1
+                </div>
+                <div className={`flex w-8 ${activeTab == 2 ? 'bg-green-400' : 'bg-slate-400'} px-12 py-2 items-center justify-center `} onClick={() => handlerTabHeadr(2)}>
+                    Tab2
+                </div>
+                <div className={`flex w-8 ${activeTab == 3 ? 'bg-green-400' : 'bg-slate-400'} px-12 py-2 items-center justify-center `} onClick={() => handlerTabHeadr(3)}>
+                    Tab3
+                </div>
+                <div className={`flex w-8 ${activeTab == 4 ? 'bg-green-400' : 'bg-slate-400'} px-12 py-2 items-center justify-center `} onClick={() => handlerTabHeadr(4)}>
+                    Tab4
+                </div>
+            </div>
+            <div className="w-full h-80 bg-slate-900">
+                div
+            </div>
         </div>
-        <div  className={`flex w-8 ${activeTab == 2 ? 'bg-green-400' : 'bg-slate-400'} px-12 py-2 items-center justify-center `} onClick={()=>handlerTabHeadr(2)}>
-          Tab2
-        </div>
-        <div  className={`flex w-8 ${activeTab == 3 ? 'bg-green-400' : 'bg-slate-400'} px-12 py-2 items-center justify-center `} onClick={()=>handlerTabHeadr(3)}>
-          Tab3
-        </div>
-        <div  className={`flex w-8 ${activeTab == 4 ? 'bg-green-400' : 'bg-slate-400'} px-12 py-2 items-center justify-center `} onClick={()=>handlerTabHeadr(4)}>
-          Tab4
-        </div>
-      </div>
-      <div className="w-full h-80 bg-slate-900">
-        div
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Tab;
