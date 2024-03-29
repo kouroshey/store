67af05c2163366a5f936283242e68ca237fc8825
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2024-03-29 11:55:25 +0330

    create and design Input comp

---
+++ src/components/common/Input.jsx
@@ -0,0 +1,24 @@
+const Input = ({
+    type = "text",
+    placeHolder = "",
+    customStyle,
+    label = "New Input",
+    id = ""
+}) => {
+
+    const inputStyle = "bg-dark-4 border-2 border-border-1 rounded-sm transition-all ease-in-out outline-none px-2 py-1 text-white focus:border-none focus:outline-4 focus:outline-white-transparent"
+
+    return (
+        <div className="flex flex-col gap-2 w-full">
+            <label htmlFor={id}>{label}</label>
+            <input
+                id={id}
+                type={type}
+                placeholder={placeHolder}
+                className={`${inputStyle} ${customStyle && customStyle}`}
+            />
+        </div>
+    )
+}
+
+export default Input
