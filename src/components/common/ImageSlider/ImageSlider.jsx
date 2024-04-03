801b4dd04a0c9851b90b5800e66bac1bdb6f30a3
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2024-04-03 20:10:52 +0330

    config react query and supabase into project

--- src/components/common/ImageSlider/ImageSlider.jsx
+++ src/components/common/ImageSlider/ImageSlider.jsx
@@ -1,13 +1,10 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-sparse-arrays */
-/* eslint-disable react/jsx-key */
+
 import React from 'react';
 import Carousel from '../Carousel-img';
 import CardSlide from './CardSlide';
 import { mainSliderDatas } from '../../../data';
 
 const ImageSlider = () => {
-  // const slides = [<CardSlide />, <CardSlide />, <CardSlide />, , <CardSlide />, <CardSlide />, <CardSlide />];
   const slides = mainSliderDatas.map((slide) => (
     <CardSlide
       key={slide.id}
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/router.jsx';
+import { QueryProvider } from './lib/react-query/QueryProvider.jsx';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+  <QueryProvider>
+    <RouterProvider router={router} />
+  </QueryProvider>
 );
