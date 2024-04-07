dfb6ac820983ed3cd0840741f60ed6510ee9516c
Kourosh Eidivandi <Kouroshjs79@gmail.com>
2024-04-07 22:03:03 +0330

    a

--- src/_auth/forms/Login.jsx
+++ src/_auth/forms/Login.jsx
@@ -5,9 +5,21 @@ export const Login = ({ setIsSignedUp }) => {
         setIsSignedUp(false)
     }
 
+    const loginHandler = async () => {
+        const { data, error } = await supabase.auth.signInWithOAuth({
+            provider: "google",
+            options: {
+                queryParams: {
+                    accessType: "offline",
+                    prompt: "consent",
+                },
+            },
+        });
+    };
+
     return (
         <div className="flex flex-col gap-4">
-            <button>login</button>
+            <button onClick={loginHandler}>login</button>
             <button>U have not register yet? <span className="text-red-500" onClick={redirectToSignup}>SignUp</span></button>
         </div>
     )
